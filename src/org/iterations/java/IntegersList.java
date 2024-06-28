8a76c7fcbc0f41bd1457c2d5b4565085657d54cb
Mario Giordano <164010608+LesPaul997@users.noreply.github.com>
2024-06-28 22:52:48 +0200

    feat class "Main"

--- src/org/iterations/java/IntegersList.java
+++ src/org/iterations/java/IntegersList.java
@@ -1,5 +1,45 @@
 package org.iterations.java;
 
 public class IntegersList {
-
+	
+	private int[] numbers;
+	private int  index;
+	
+	public IntegersList(int[] numbers) {
+		this.numbers = numbers;
+	}
+	
+	public IntegersList() {
+		this.numbers = new int[0];
+	}
+	
+	public int getElementoSuccessivo() {
+		int value = 0;
+		if (this.index < numbers.length) {
+			value = numbers[index];
+			this.index++;
+		}
+		return value;
+	}
+	
+	public boolean hasAncoraElementi() {
+		return (this.index < numbers.length);
+	}
+	
+	public void addElemento(int newNumber) {
+		int[] newNumbers = new int[this.numbers.length + 1];
+		for (int i = 0; i < numbers.length; i++) {
+			newNumbers[i] = numbers[i];
+		}
+		newNumbers[numbers.length] = newNumber;
+		this.numbers = newNumbers;
+	}
+	
+	public int[] getNumbers() {
+		return this.numbers;
+	}
+	
+	public int getIndex() {
+		return this.index;
+	}
 }
--- src/org/iterations/java/Main.java
+++ src/org/iterations/java/Main.java
@@ -1,5 +1,33 @@
 package org.iterations.java;
 
+import java.util.Arrays;
+
 public class Main {
 
+	public static void main(String[] args) {
+		//Istanza di IntegersList inizializzata con un array di interi
+		IntegersList listaInteri = new IntegersList(new int[] {22,56,35,98,5});
+		
+		//Estrazione degli elementi
+		System.out.println(listaInteri.getElementoSuccessivo());
+        System.out.println(listaInteri.getElementoSuccessivo());
+        System.out.println(listaInteri.getElementoSuccessivo());
+        
+        //Verifica della presenza di elementi
+        System.out.println(listaInteri.hasAncoraElementi());
+        
+        //Estrazione degli altri elementi
+        System.out.println(listaInteri.getElementoSuccessivo());
+        System.out.println(listaInteri.getElementoSuccessivo());
+        
+        //Verifica e tentativo di estrazione con valore 0
+        System.out.println(listaInteri.getElementoSuccessivo());
+        System.out.println(listaInteri.getElementoSuccessivo());
+        
+        //istanza del secondo oggetto partendo da un array vuoto
+        IntegersList secondaLista = new IntegersList();
+        
+        //Verifica della lista vuota
+        System.out.println(secondaLista.hasAncoraElementi());
+	}
 }
